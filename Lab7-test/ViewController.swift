fb83541bfe5c2049c2b508330da8dd4aa8f61cd6
Sean Wang <seanwang0111@gmail.com>
2021-02-07 21:04:22 +0800

    Initial Commit

---
+++ Lab7-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab7-test
+//
+//  Created by Sean Wang on 2021/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
